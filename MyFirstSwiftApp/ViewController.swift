420a06bd57c22624108b429b4a2e08cacfbce0ca
Vasudha Subramanian <subramanian.vasudha@gmail.com>
2019-07-17 11:47:02 -0700

    Switching between 2 images

--- MyFirstSwiftApp/ViewController.swift
+++ MyFirstSwiftApp/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var imageView1: UIImageView!
+    var showFirstImage = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonClick(_ sender: Any) {
+        if (showFirstImage == false){
+            imageView1.image = UIImage(named: "Bear")
+            showFirstImage = true
+        } else {
+            imageView1.image = UIImage(named: "selected_star")
+            showFirstImage = false
+        }
+
+    }
 }
 
